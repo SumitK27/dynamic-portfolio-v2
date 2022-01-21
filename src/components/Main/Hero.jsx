a363b4be2c15bc2e4d80f642160ad1f0c347cf0c
SumitK27 <sumitkajbaje@gmail.com>
2022-01-21 23:29:37 +0530

    Added Hero Section

--- src/components/Main/Hero.jsx
+++ src/components/Main/Hero.jsx
@@ -1,50 +1,43 @@
 import React from "react";
 
-const Hero = () => {
+const Hero = ({ rotate, heroImg }) => {
     return (
         <div className="mb-4 container-fluid">
-            {/* row */}
             <div className="row p-60-0 p-lg-30-0 p-md-15-0">
-                {/* col */}
                 <div className="col-lg-12">
-                    {/* banner */}
                     <div
                         className="art-a art-banner"
                         style={{
                             backgroundImage: "url(img/bg.jpg)",
                         }}
                     >
-                        {/* banner back */}
                         <div className="art-banner-back" />
-                        {/* banner dec */}
+
                         <div className="art-banner-dec" />
-                        {/* banner overlay */}
+
                         <div className="art-banner-overlay">
-                            {/* main title */}
                             <div className="art-banner-title">
-                                {/* title */}
                                 <h1 className="mb-15">
                                     Discover my Amazing <br />
                                     Art Space!
                                 </h1>
-                                {/* suptitle */}
+
                                 <div className="art-lg-text art-code mb-25">
                                     &lt;<i>code</i>
                                     &gt; I build{" "}
                                     <span
                                         className="txt-rotate"
                                         data-period={2000}
-                                        data-rotate='[ "web interfaces.", "ios and android applications.", "design mocups.", "automation tools." ]'
+                                        data-rotate={JSON.stringify(rotate)}
                                     />
                                     &lt;/<i>code</i>
                                     &gt;
                                 </div>
                                 <div className="art-buttons-frame">
-                                    {/* button */}
                                     <a href="#." className="art-btn art-btn-md">
                                         <span>Explore now</span>
                                     </a>
-                                    {/* button */}
+
                                     <a
                                         href="#."
                                         className="art-link art-white-link art-w-chevron"
@@ -53,21 +46,16 @@ const Hero = () => {
                                     </a>
                                 </div>
                             </div>
-                            {/* main title end */}
-                            {/* photo */}
+
                             <img
-                                src="img/face-2.png"
+                                src={heroImg}
                                 className="art-banner-photo"
-                                alt="Your Name"
+                                alt="Hero"
                             />
                         </div>
-                        {/* banner overlay end */}
                     </div>
-                    {/* banner end */}
                 </div>
-                {/* col end */}
             </div>
-            {/* row end */}
         </div>
     );
 };
