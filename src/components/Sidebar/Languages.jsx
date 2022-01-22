66e340b01cd42e599cd5d2ac351c42f99ebc80fd
SumitK27 <sumitkajbaje@gmail.com>
2022-01-22 22:43:30 +0530

    Added Dynamic progressbar

--- src/components/Sidebar/Languages.jsx
+++ src/components/Sidebar/Languages.jsx
@@ -6,9 +6,47 @@ const Languages = ({ languages }) => {
             {languages.map((language, i) => (
                 <div className="art-lang-skills-item" key={language.name}>
                     <div
-                        id={`circleprog${i + 1}`}
                         className="art-circle-progress"
-                    />
+                        style={{ position: "relative" }}
+                    >
+                        <svg
+                            viewBox="0 0 100 100"
+                            style={{ display: "block", width: "100%" }}
+                        >
+                            <path
+                                d="M 50,50 m 0,-46.5 a 46.5,46.5 0 1 1 0,93 a 46.5,46.5 0 1 1 0,-93"
+                                stroke="#eee"
+                                stroke-width="7"
+                                fill-opacity="0"
+                            ></path>
+                            <path
+                                d="M 50,50 m 0,-46.5 a 46.5,46.5 0 1 1 0,93 a 46.5,46.5 0 1 1 0,-93"
+                                stroke="#555"
+                                stroke-width="7"
+                                fill-opacity="0"
+                                style={{
+                                    "stroke-dasharray": `${
+                                        (360 / 100) * language.score
+                                    }, ${(360 / 100) * language.score}`,
+                                    "stroke-dashoffset": "29.2273",
+                                }}
+                            ></path>
+                        </svg>
+                        <div
+                            class="progressbar-text"
+                            style={{
+                                position: "absolute",
+                                left: "50%",
+                                top: "50%",
+                                padding: "0px",
+                                margin: "0px",
+                                transform: "translate(-50%, -50%)",
+                                color: "rgb(85, 85, 85)",
+                            }}
+                        >
+                            {language.score}
+                        </div>
+                    </div>
                     <h6>{language.name}</h6>
                 </div>
             ))}
